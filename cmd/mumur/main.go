dbc5e4dde7b6278de7e6b4e5bc7e2cc83df76c14
Edwin Jose <jose.edwin997@gmail.com>
2022-06-11 20:07:24 -0700

    chore(main) : Add charm example

--- cmd/mumur/main.go
+++ cmd/mumur/main.go
@@ -20,28 +20,28 @@ func main() {
 	}
 }
 
-func (m model) View() string {
-	s := "What should we buy at the market?\n\n"
-	for i, choice := range m.choices {
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-	}
-	s += "\nPress q to quit.\n"
-	return s
-}
-
 func initialModel() model {
 	return model{
 		choices:  []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
 		selected: make(map[int]struct{}),
 	}
+}
+
+func (m model) View() string {
+	s := "What should we buy at the market?\n\n"
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+	s += "\nPress q to quit.\n"
+	return s
 }
 
 func (m model) Init() tea.Cmd {
